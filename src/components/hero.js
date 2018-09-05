3c2505875750baca4ccff454191983d8ef4c7c36
Caitlin <caitlin.ch.johnson@gmail.com>
2018-09-05 09:23:48 -0700

    added the hero and portfolio item placeholder sections

---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './hero.css';
+
+class Hero extends Component {
+  render() {
+    return (
+      <hero-container>
+        <h1>CAITLIN C. JOHNSON</h1>
+        <p>Full-Stack Development | Global Health | Innovation</p>
+        <button type="button">About Me</button>
+      </hero-container>
+    );
+  }
+}
+
+export default Hero;
---
+++ src/components/portfolio-section.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './portfolio-section.css';
+
+class PortfolioSection extends Component {
+  render() {
+    return (
+      <section>
+        <h2>Full-Stack Development Portfolio</h2>
+        <display-items>
+          Portfolio Items Go Here
+        </display-items>
+      </section>
+    );
+  }
+}
+
+export default PortfolioSection;
