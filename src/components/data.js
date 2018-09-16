cc4c19a0092dc0ddef1a86bb50065725a8bb81e4
Caitlin <caitlin.ch.johnson@gmail.com>
2018-09-16 12:12:51 -0700

    created an internal api document of sorts

---
+++ src/components/data.js
@@ -0,0 +1,93 @@
+export default {
+    "languages": [
+    {
+      id: 1,
+      name: 'Python',
+      levelOfExperience: 70
+    },
+    {
+      id: 2,
+      name: 'Ruby',
+      levelOfExperience: 50
+    },
+    {
+      id: 3,
+      name: 'JavaScript',
+      levelOfExperience: 80
+    },
+    {
+      id: 4,
+      name: 'SQL',
+      levelOfExperience: 40
+    },
+    {
+      id: 5,
+      name: 'HTML',
+      levelOfExperience: 70
+    },
+    {
+      id: 6,
+      name: 'CSS',
+      levelOfExperience: 80
+    }
+  ],
+  "frameworks": [
+    {
+      id: 1,
+      name: 'Ruby on Rails',
+      levelOfExperience: 70
+    },
+    {
+      id: 2,
+      name: 'ReactJS',
+      levelOfExperience: 70
+    },
+    {
+      id: 3,
+      name: 'NumPy / SciPy / Pandas',
+      levelOfExperience: 60
+    },
+    {
+      id: 4,
+      name: 'Bootstrap',
+      levelOfExperience: 40
+    }
+  ],
+  "technologies": [
+    {
+      id: 1,
+      name: 'Jupyter Notebook',
+      levelOfExperience: 40
+    },
+    {
+      id: 2,
+      name: 'Atom',
+      levelOfExperience: 80
+    },
+    {
+      id: 3,
+      name: 'Anaconda',
+      levelOfExperience: 40
+    },
+    {
+      id: 4,
+      name: 'Microsoft Visual Studio',
+      levelOfExperience: 30
+    },
+    {
+      id: 5,
+      name: 'Git and Github',
+      levelOfExperience: 70
+    },
+    {
+      id: 6,
+      name: 'Terminal',
+      levelOfExperience: 80
+    },
+    {
+      id: 7,
+      name: 'Orange3',
+      levelOfExperience: 30
+    }
+  ]
+}
--- src/components/sections/skills-section.js
+++ src/components/sections/skills-section.js
@@ -1,107 +1,48 @@
 import React, { Component } from 'react';
 import { ProgressBar } from 'react-bootstrap';
+import data from '../data';
 
 class SkillsSection extends Component {
 
   constructor(props){
     super(props)
-    this.state = [
-      {
-        id: 1,
-        language: 'Python',
-        levelOfExperience: 3
-      },
-      {
-        id: 2,
-        language: 'Ruby',
-        levelOfExperience: 3
-      },
-      {
-        id: 3,
-        language: 'JavaScript',
-        levelOfExperience: 4
-      },
-      {
-        id: 4,
-        language: 'SQL',
-        levelOfExperience: 2
-      },
-      {
-        id: 5,
-        language: 'HTML',
-        levelOfExperience: 3
-      },
-      {
-        id: 6,
-        language: 'CSS',
-        levelOfExperience: 3
-      }
-    ],[
-      {
-        id: 1,
-        framework: 'Ruby on Rails',
-        levelOfExperience: 3
-      },
-      {
-        id: 2,
-        framework: 'ReactJS',
-        levelOfExperience: 3
-      },
-      {
-        id: 3,
-        framework: 'NumPy',
-        levelOfExperience: 3
-      },
-      {
-        id: 4,
-        framework: 'SciPy',
-        levelOfExperience: 3
-      },
-      {
-        id: 5,
-        framework: 'Pandas',
-        levelOfExperience: 3
-      },
-      {
-        id: 6,
-        framework: 'Orange3',
-        levelOfExperience: 3
-      }
-    ],[
-      {
-        id: 1,
-        technology: 'Jupyter Notebook',
-        levelOfExperience: 3
-      },
-      {
-        id: 2,
-        technology: 'Atom',
-        levelOfExperience: 3
-      },
-      {
-        id: 3,
-        technology: 'Github/Git',
-        levelOfExperience: 3
-      },
-      {
-        id: 4,
-        technology: 'Anaconda',
-        levelOfExperience: 3
-      },
-      {
-        id: 5,
-        technology: 'Atom',
-        levelOfExperience: 3
-      },
-      {
-        id: 6,
-        technology: 'Microsoft Visual Studio',
-        levelOfExperience: 3
-      }
-    ]
+    this.state = {
+      languages: data.languages,
+      frameworks: data.frameworks,
+      technologies: data.technologies
   }
+}
 
   render() {
+    let { languages, frameworks, technologies } = this.state
+
+    let languageBars = this.state.languages.map((language) => {
+      return (
+        <div>
+          <p>{language.name}</p>
+          <ProgressBar bsStyle="info" now={language.levelOfExperience} label={`${language.levelOfExperience}%`}/>
+        </div>
+      )
+    })
+
+    let frameworkBars = this.state.frameworks.map((framework) => {
+      return (
+        <div>
+          <p>{framework.name}</p>
+          <ProgressBar bsStyle="success" now={framework.levelOfExperience} label={`${framework.levelOfExperience}%`}/>
+        </div>
+      )
+    })
+
+    let technologyBars = this.state.technologies.map((technology) => {
+      return (
+        <div>
+          <p>{technology.name}</p>
+          <ProgressBar bsStyle="danger" now={technology.levelOfExperience} label={`${technology.levelOfExperience}%`}/>
+        </div>
+      )
+    })
+
     return (
       <section>
         <h2 className="header">Skills</h2>
@@ -112,18 +53,7 @@ class SkillsSection extends Component {
             <p>While I am more skilled on the front end, I align more closely with back end languages, specifically with Python due to my extended experience with the language and ongoing education relating to data visualization and machine learning. My SQL experience has been primarily focused on Postgres and MongoDB.</p>
           </div>
           <div className="col-md-8 skills-level-box">
-            <p>Python</p>
-            <ProgressBar bsStyle="info" now={70} label={`70%`}/>
-            <p>Ruby</p>
-            <ProgressBar bsStyle="info" now={50} label={`50%`}/>
-            <p>JavaScript</p>
-            <ProgressBar bsStyle="info" now={80} label={`80%`}/>
-            <p>SQL</p>
-            <ProgressBar bsStyle="info" now={40} label={`40%`}/>
-            <p>HTML</p>
-            <ProgressBar bsStyle="info" now={70} label={`70%`}/>
-            <p>CSS</p>
-            <ProgressBar bsStyle="info" now={80} label={`80%`}/>
+            {languageBars}
           </div>
         </div>
         <div className="container" style={{display: "flex", flexDirection: "row"}}>
@@ -132,14 +62,7 @@ class SkillsSection extends Component {
             <p>This is by no means an exhaustive list, but these libraries and frameworks are the ones that I am most comfortable with. Going through full stack web development bootcamp gave me exposure to React.js and Ruby on Rails; however, I have gained exposure to NumPy, SciPy, and Pandas through my Data Science & Machine Learning courses.</p>
           </div>
           <div className="col-md-8 skills-level-box">
-            <p>Ruby on Rails</p>
-            <ProgressBar bsStyle="success" now={70} label={`70%`}/>
-            <p>React.js</p>
-            <ProgressBar bsStyle="success" now={70} label={`70%`}/>
-            <p>NumPy / SciPy / Pandas</p>
-            <ProgressBar bsStyle="success" now={60} label={`60%`}/>
-            <p>Bootstrap</p>
-            <ProgressBar bsStyle="success" now={40} label={`40%`}/>
+            {frameworkBars}
           </div>
         </div>
         <div className="container" style={{display: "flex", flexDirection: "row"}}>
@@ -148,20 +71,7 @@ class SkillsSection extends Component {
             <p>Going through full stack web development bootcamp and supplemental courses relating to data science and machine learning have given me exposure to multiple tools.</p>
           </div>
           <div className="col-md-8 skills-level-box">
-            <p>Jupyter Notebook</p>
-            <ProgressBar bsStyle="danger" now={40} label={`40%`}/>
-            <p>Atom</p>
-            <ProgressBar bsStyle="danger" now={80} label={`80%`}/>
-            <p>Anaconda</p>
-            <ProgressBar bsStyle="danger" now={40} label={`40%`}/>
-            <p>Microsoft Visual Studio</p>
-            <ProgressBar bsStyle="danger" now={30} label={`30%`}/>
-            <p>Git and Github</p>
-            <ProgressBar bsStyle="danger" now={70} label={`70%`}/>
-            <p>Terminal</p>
-            <ProgressBar bsStyle="danger" now={80} label={`80%`}/>
-            <p>Orange3</p>
-            <ProgressBar bsStyle="danger" now={30} label={`30%`}/>
+            {technologyBars}
           </div>
         </div>
       </section>
